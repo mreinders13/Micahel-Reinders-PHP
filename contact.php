9b8182105a9799ebe8fc890906a72f1f46590e57
mreinders13 <michael.h.reinders@gmail.com>
2019-01-25 16:31:45 -0800

    first commit

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php include("inc/header.php"); ?>
+<h1 style="font-weight: 100;">Contact</h1>
+
+<p style="text-align: center;">
+	<a href="mailto:michael.h.reinders@gmail.com"><img class="clickable-icon" src="img/email.png" alt="Email" /></a>
+	<a href="https://www.linkedin.com/in/michaelhreinders/" target="_blank"><img class="clickable-icon" target="_blank" src="img/linkedin.png" alt="LinkedIn" /></a>
+	<a href="https://github.com/mreinders13" target="_blank"><img class="clickable-icon" target="_blank" src="img/github.png" alt="GitHub" /></a>
+</p>
+<?php include("inc/footer.php"); ?>
---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Michael Reinders</title>
+	<meta name="description" content="Programming and Web Design"><meta name="author" content="Michael Reinders"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" />
+	<link href="css/normalize.css" rel="stylesheet" type="text/css" />
+	<link href="css/Main.css" rel="stylesheet" type="text/css" />
+	<link href="css/screenResponse.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="siteTitle">
+<p>Michael-Reinders.com</p>
+</div>
+
+<div id="nav-bar">
+<nav><a class="nav-class, active" href="/">Home</a> <a class="nav-class" href="/about.php">About</a> <a class="nav-class" href="/portfolio.php">Portfolio</a> <a class="nav-class" href="/contact.php">Contact</a></nav>
+</div>
+<!-- Set all 3 Images to change in responsive CSS -->
+ 
+<img alt="Programming and Web Design" id="home-banner" src="img/Michael-Reinders-Main-Banner.png" /> 
+<img alt="Programming and Web Design" id="home-banner-800" src="img/Michael-Reinders-Main-Banner-800.png" /> 
+<img alt="Programming and Web Design" id="home-banner-480" src="img/Michael-Reinders-Main-Banner-480.png" /> 
+
+<!-- Main Content -->
+<div id="page-main-content">
